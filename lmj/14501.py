78b6af3d360b72476f3cbacda0ea8dc423cd5641
minjung2266 <dlalswjd226@naver.com>
2025-06-02 01:33:42 +0900

    boj : 14501 complete

--- lmj/14501.py
+++ lmj/14501.py
@@ -1,4 +1,5 @@
-n =int(input())
+# 퇴사
+n = int(input())
 
 days = [list(map(int, input().split())) for _ in range(n)]
 dp = [0]*(n+1)
@@ -7,4 +8,5 @@ for i in range(n-1, -1, -1):
         dp[i] = max(days[i][1] + dp[i+days[i][0]], dp[i+1])
     else:
         dp[i] = dp[i+1]
+        
 print(dp[0])
